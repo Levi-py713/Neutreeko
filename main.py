b322ecb40ee978aa450a77803061a1b0a823a8b2
Levi <levi_serrano@berkeley.edu>
2026-09-21 19:13:24 -0700

    Text User Interface Pt.1

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-class Piece():
+import os
+class Board():
     moveSet = {'N': [-1, 0], 'E': [0, 1], 'S': [1, 0], 'W': [0, -1], 'NE': [-1, 1], 'NW': [-1, -1], 'SE': [1, 1], 'SW': [1, -1]}
+
+    white_position = [[0, 1], [1, 3], [3, 2]]
+    black_position = [[1, 2], [4, 1], [4, 3]]
+    
+    game_board = [
+                    ['_', 'W1', '_', 'W2', '_'],
+                    ['_', '_', 'B1', '_', '_'],
+                    ['_', '_', '_', '_', '_'],
+                    ['_', '_', 'W3', '_', '_'],
+                    ['_', 'B2', '_', 'B3', '_']]
+
+    def __str__(self):
+        for x in Board.game_board:
+            print(x)
+        return ' '
     # position on board, 'white' or 'black'
     # def __init__(self, position, color):
     #     self.position = position
@@ -10,17 +26,18 @@ class Piece():
 
     #Checks if you can move in that direction
     def canMove(self, position):
-        if position[0] > 4 or position[0] < 0 or position[1] > 4 or position[1] < 0:
+        if position[0] > len(Board.game_board) or position[0] < 0 or position[1] > len(Board.game_board) or position[1] < 0:
             return False
         elif Board.game_board[position[0]][position[1]]!= '_': 
             return False
         else:
             return True
             # Check if position is primitive, else moves position
-    def DoMove(self, position: list[int], move: str): 
+
+    def DoMove(self, name:str, position: list[int], move: str): 
 
 
-        current_position = position
+        current_position = [position[0], position[1]]
 
         while self.canMove([position[0] + self.moveSet[move][0], position[1] + self.moveSet[move][1]]):
             position[0] += self.moveSet[move][0]
@@ -28,16 +45,13 @@ class Piece():
             print(position)
 
         Board.game_board[current_position[0]][current_position[1]] = '_'
-        Board.game_board[position[0]][position[1]] = 'XX'
+        Board.game_board[position[0]][position[1]] = name
 
         return "Moved"
 
         # Pre-scan before making move
         # 1. Fetch position and direction - if want to move 'north' check all squares ABOVE current position
         # 2. Loop over JUST that column and check
-
-        # while Board.game_board[position[0]][position[1]] != 
-
 
     # Scaffolding
 
@@ -46,7 +60,7 @@ class Piece():
         if self.IsPrimitive() == 'primitive':
             return 'primitive'
         else:
-            for move in Piece.moveSet.keys():
+            for move in self.moveSet.keys():
                 if self.canMove([position[0] + self.moveSet[move][0], position[1] + self.moveSet[move][1]]):
                     moves.append(move)
         return moves
@@ -71,38 +85,23 @@ class Piece():
                 elif (sorted_list[0][0] + 1 == sorted_list[1][0] and sorted_list[1][0] + 1 == sorted_list[2][0]) and (sorted_list[0][1] + 1 == sorted_list[1][1] and sorted_list[1][1] + 1 == sorted_list[2][1]):
                     return True
         return False
-            
-        
-
-
-
-
-
-        index = [0, 0]
-            
-
-        raise NotImplementedError()
-
-
-class Board():
-    white_position = [[0, 1], [1, 3], [2, 2]] # [0, 1], [1, 3], [2, 2]
-    black_position = [[1, 2], [4, 1], [4, 3]] # [1, 2], [4, 1], [4, 3]
-    
-    game_board = [
-                    ['_', 'O', '_', 'O', '_'],
-                    ['_', '_', 'X', '_', '_'],
-                    ['_', '_', '_', '_', '_'],
-                    ['_', '_', 'O', '_', '_'],
-                    ['_', 'X', '_', 'X', '_']]
-
-    def __str__(self):
-        for x in Board.game_board:
-            print(x)
-        return ' '
 
 def main():
+    turn = "black"
+    not_turn = "white"
     x = Board()
-    y = Piece()
-    print(y.IsPrimitive())
+    while not x.IsPrimitive():
+        print(x)
+        move = input(f'Turn: {turn}, pick a piece and move (e.g B1N): ')
+        os.system('clear')
+        if turn == 'black':
+            x.DoMove(move[:2], x.black_position[int(move[1]) - 1], move[2:])
+        else:
+            x.DoMove(move[:2], x.white_position[int(move[1]) - 1], move[2:])
+        turn, not_turn = not_turn, turn
+    print(x)
+    print(f'{not_turn} has won!')
+
+
 
 main()
